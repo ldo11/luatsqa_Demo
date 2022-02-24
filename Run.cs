9f1ef22e43a78f96721048215613964cafd84950
Do Luat <doluat11@gmail.com>
2022-02-24 15:06:16 -0600

    add first test case

---
+++ Run.cs
@@ -0,0 +1,38 @@
+﻿using luatsqa;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    public class Run
+    {
+        public Run(string id)
+        {
+            SetVariables();
+            try
+            {
+                Testcase tc = new Testcase(id);
+                TCResult r = tc.Execute();
+                TestContext.AddTestAttachment(r.report);
+                System.Diagnostics.Process.Start(@"cmd.exe ", @"/c " + r.report);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public void SetVariables()
+        {
+            Environment.SetEnvironmentVariable("Ldomain", "https://demo.luatsqa.com/");
+            Environment.SetEnvironmentVariable("Lusername", "demo");
+            Environment.SetEnvironmentVariable("Lpassword", "demo");
+            Environment.SetEnvironmentVariable("Lenv", "1");//1-TEST, 2-INTG, 3-UAT, 4-PROD
+            Environment.SetEnvironmentVariable("Lteam", "demo");
+
+        }
+    }
+}
--- UnitTest1.cs
+++ UnitTest1.cs
@@ -9,6 +9,7 @@ namespace Demo
         [TestMethod]
         public void TestMethod1()
         {
+            Run r = new Run("ee4f7172-9596-11ec-bb26-001a4a250076");
         }
     }
 }
