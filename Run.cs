48229b3ec709cd191acdcaecbb7a71887fba5dcb
Do Luat <doluat11@gmail.com>
2022-03-16 22:29:08 -0500

    update more test cases

--- Run.cs
+++ Run.cs
@@ -10,14 +10,23 @@ namespace Demo
 {
     public class Run
     {
-        public Run(string id)
+        public Run(string id, string testdatafile=null)
         {
             SetVariables();
             try
             {
-                Testcase tc = new Testcase(id);
+                Testcase tc = new Testcase(id, testdatafile);
                 TCResult r = tc.Execute();
                 TestContext.AddTestAttachment(r.report);
+                TestContext.AddTestAttachment(r.log);
+                if(r.result == 1)
+                {
+                    Assert.Pass("Test Case PASSED");
+                }
+                else
+                {
+                    Assert.Fail("Test Case FAILED");
+                }
                 System.Diagnostics.Process.Start(@"cmd.exe ", @"/c " + r.report);
             }
             catch (Exception ex)
@@ -27,11 +36,14 @@ namespace Demo
         }
         public void SetVariables()
         {
+
             Environment.SetEnvironmentVariable("Ldomain", "https://demo.luatsqa.com/");
             Environment.SetEnvironmentVariable("Lusername", "demo");
             Environment.SetEnvironmentVariable("Lpassword", "demo");
             Environment.SetEnvironmentVariable("Lenv", "1");//1-TEST, 2-INTG, 3-UAT, 4-PROD
             Environment.SetEnvironmentVariable("Lteam", "demo");
+            Environment.SetEnvironmentVariable("a1", "foo2");
+            Environment.SetEnvironmentVariable("a2", "bar");
 
         }
     }
--- UnitTest1.cs
+++ UnitTest1.cs
@@ -9,7 +9,24 @@ namespace Demo
         [TestMethod]
         public void TestMethod1()
         {
-            Run r = new Run("ee4f7172-9596-11ec-bb26-001a4a250076");
+            Run r = new Run("ee4f7172-9596-11ec-bb26-001a4a250076", "121212");
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Run r = new Run("4b7abb94-95a1-11ec-bb26-001a4a250076");
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Run r = new Run("214b2963-98a0-11ec-bb26-001a4a250076");
+        }
+        //12f7d353-98b8-11ec-bb26-001a4a250076
+        [TestMethod]
+        public void TestMethod4()
+        {
+            //Run r = new Run("12f7d353-98b8-11ec-bb26-001a4a250076");
+            Run r2 = new Run("12f7d353-98b8-11ec-bb26-001a4a250076", "121212");
         }
     }
 }
